b7299c1c66b4af6b68dd8771cbea4a4e8237f22f
honokaaa <ec2-user@ip-172-31-4-201.us-east-2.compute.internal>
2018-06-04 06:08:45 +0000

    naota

--- database/migrations/2018_06_04_150506_create_tasklists_table.php
+++ database/migrations/2018_06_04_150506_create_tasklists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStatusToTasklistsTable extends Migration
+class CreateTasklistsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class AddStatusToTasklistsTable extends Migration
      */
     public function up()
     {
-        Schema::table('tasklists', function (Blueprint $table) {
-            //
+        Schema::create('tasklists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content'); 
+            $table->timestamps();
         });
     }
 
@@ -25,8 +27,6 @@ class AddStatusToTasklistsTable extends Migration
      */
     public function down()
     {
-        Schema::table('tasklists', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('tasklists');
     }
 }

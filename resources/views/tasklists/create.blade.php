26c183ed6853bb62091f2bc276e1f9dcc6364dce
honokaaa <ec2-user@ip-172-31-4-201.us-east-2.compute.internal>
2018-06-07 00:20:58 +0000

    bootstrap

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -8,7 +8,7 @@
     
    <div class="row">
         
-         <div class="col-xs-offset-12 col-sm-offset-8 col-lg-offset-3">
+         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6"
         
             {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
             
